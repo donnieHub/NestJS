0ae872cec1fac894d8fa26f58b511ed7e549f741
Vladimir Mychko <vmychko@rvision.ru>
2025-06-27 16:51:58 +0600

    remove rooms-service module

--- r-omni/apps/rooms-service/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { RoomsServiceModule } from './rooms-service.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(RoomsServiceModule);
-  await app.listen(process.env.port ?? 3000);
-}
-bootstrap();
--- r-omni/apps/rooms-service/src/rooms-service.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class RoomsServiceService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-}
