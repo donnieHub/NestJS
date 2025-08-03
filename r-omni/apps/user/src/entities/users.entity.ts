66b235ecb285ad201d8bd22b57b00d1d6f50d0ef
Vladimir Mychko <vmychko@rvision.ru>
2025-08-03 20:04:24 +0600

    add user.repository.ts

--- r-omni/apps/user/src/entities/users.entity.ts
+++ r-omni/apps/user/src/entities/users.entity.ts
@@ -1,4 +1,4 @@
-import {Entity, EntityRepositoryType, PrimaryKey, Property, Unique} from '@mikro-orm/core';
+import {Entity, EntityRepositoryType, PrimaryKey, Property} from '@mikro-orm/core';
 import {v4} from "uuid";
 import {UserRepository} from "../user.repository";
 
