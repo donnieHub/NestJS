6ed3e3399881bdeafc5911f9d8ecae8da544532f
Vladimir Mychko <vmychko@rvision.ru>
2025-08-27 22:21:28 +0600

    add booking mikro-orm.config.ts

---
+++ r-omni/apps/booking/mikro-orm.config.ts
@@ -0,0 +1,25 @@
+import { Options } from '@mikro-orm/core';
+import {PostgreSqlDriver} from "@mikro-orm/postgresql";
+import * as path from "node:path";
+import * as dotenv from 'dotenv';
+import {Booking} from "./src/entities/bookings.entity";
+
+dotenv.config();
+
+export const config: Options<PostgreSqlDriver> = {
+    entities: [Booking],
+    entitiesTs: [Booking],
+    dbName: process.env.DB_NAME || 'booking_service_db',
+    host: process.env.DB_HOST || 'localhost',
+    port: parseInt(process.env.BOOKING_DB_PORT || '54321', 10),
+    user: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASSWORD || 'postgres',
+    driver: PostgreSqlDriver,
+    migrations: {
+        path: path.resolve(__dirname, './src/migrations'),
+        pathTs: path.resolve(__dirname, './src/migrations'),
+    },
+    debug: process.env.DB_DEBUG === 'true' || true,
+};
+
+export default config;
