f053e4fe16cf0588564aa17c403e99e9fb2c825a
Vladimir Mychko <vmychko@rvision.ru>
2025-08-31 22:58:29 +0600

    fix rooms.module.ts

--- r-omni/apps/rooms/src/rooms.module.ts
+++ r-omni/apps/rooms/src/rooms.module.ts
@@ -3,9 +3,9 @@ import { RoomsController } from './rooms.controller';
 import { RoomsService } from './rooms.service';
 import {ConfigModule} from "@nestjs/config";
 import {MikroOrmModule} from "@mikro-orm/nestjs";
-import {Room} from "../../r-omni/src/room.model";
 import {config} from "../mikro-orm.config";
 import {RoomAvailability} from "./entities/room.availability.entity";
+import {Room} from "./entities/rooms.entity";
 
 @Module({
   imports: [
